39a9f85061b13dfa8b1e3a6ea4cbe9df81a022bb
wanchai2540 <58947369+wanchai2540@users.noreply.github.com>
2020-07-30 00:23:07 +0700

    -TestTwoGame

---
+++ app/src/main/java/com/example/testgame/MainActivity.kt
@@ -0,0 +1,144 @@
+package com.example.testgame
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+    private val buttons = Array(3) { arrayOfNulls<Button>(3) }
+    private var player1Turn = true
+    private var roundCount = 0
+    private var player1Points = 0
+    private var player2Points = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initview()
+    }
+
+    private fun initview() {
+        for (i in 0..2) {
+            for (j in 0..2) {
+                val buttonID = "button_$i$j"
+                val resID = resources.getIdentifier(buttonID, "id", packageName)
+                buttons[i][j] = findViewById(resID)
+                buttons[i][j]!!.setOnClickListener(this)
+            }
+        }
+
+        button_reset.setOnClickListener {resetGame() }
+    }
+
+    override fun onClick(v: View?) {
+        if ((v as Button).text.toString() != "") {
+            return
+        }
+        if (player1Turn) {
+            v.text = "X"
+        } else {
+            v.text = "O"
+        }
+        roundCount++
+        if (checkForWin()) {
+            if (player1Turn) {
+                player1Wins()
+            } else {
+                player2Wins()
+            }
+        } else if (roundCount == 9) {
+            draw()
+        } else {
+            player1Turn = !player1Turn
+        }
+
+    }
+
+    private fun checkForWin(): Boolean {
+        val field = Array(3) { arrayOfNulls<String>(3) }
+        for (i in 0..2) {
+            for (j in 0..2) {
+                field[i][j] = buttons[i][j]!!.text.toString()
+            }
+        }
+        for (i in 0..2) {
+            if (field[i][0] == field[i][1] && field[i][0] == field[i][2] && field[i][0] != ""
+            ) {
+                return true
+            }
+        }
+        for (i in 0..2) {
+            if (field[0][i] == field[1][i] && field[0][i] == field[2][i] && field[0][i] != ""
+            ) {
+                return true
+            }
+        }
+        if (field[0][0] == field[1][1] && field[0][0] == field[2][2] && field[0][0] != ""
+        ) {
+            return true
+        }
+        return field[0][2] == field[1][1] && field[0][2] == field[2][0] && field[0][2] != ""
+    }
+
+    private fun player1Wins() {
+        player1Points++
+        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show()
+        updatePointsText()
+        resetBoard()
+    }
+
+    private fun player2Wins() {
+        player2Points++
+        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show()
+        updatePointsText()
+        resetBoard()
+    }
+
+    private fun draw() {
+        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show()
+        resetBoard()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updatePointsText() {
+        text_view_p1!!.text = "Player 1: $player1Points"
+        text_view_p2!!.text = "Player 2: $player2Points"
+    }
+
+    private fun resetBoard() {
+        for (i in 0..2) {
+            for (j in 0..2) {
+                buttons[i][j]!!.text = ""
+            }
+        }
+        roundCount = 0
+        player1Turn = true
+    }
+
+    private fun resetGame() {
+        player1Points = 0
+        player2Points = 0
+        updatePointsText()
+        resetBoard()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("roundCount", roundCount)
+        outState.putInt("player1Points", player1Points)
+        outState.putInt("player2Points", player2Points)
+        outState.putBoolean("player1Turn", player1Turn)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        roundCount = savedInstanceState.getInt("roundCount")
+        player1Points = savedInstanceState.getInt("player1Points")
+        player2Points = savedInstanceState.getInt("player2Points")
+        player1Turn = savedInstanceState.getBoolean("player1Turn")
+    }
+}
